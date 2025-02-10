d32262974f85347d12194b33feb2672ae50bb630
NICOLAS ROCK PESSOA <60918958342@icc.local>
2025-02-10 13:24:00 -0300

    Atualizando e testando o tailwindcss, informa q n esta declarado mais esta funcionando

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom'
 
 export function Login() {
     return (
-      <div className="flex w-full h-screen justify-center flex-col">
+      <div className="flex w-full h-screen flex-col justify-center items-center">
           <Link to='/'>
             <h1 className='mt-11 text-white mb-7 font-bold text-5xl'>Dev
               <span className='bg-gradient-to-r from-yellow-500 to-orange-400 bg-clip-text text-transparent'>Link</span>
